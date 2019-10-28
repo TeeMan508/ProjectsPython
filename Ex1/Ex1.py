c0bebcdc514f0b1446a0ca7ac5d76142ea0cc1ec
TimofeySh <TimofeySh@users.noreply.github.com>
2019-10-28 17:20:48 +0700

    Initial commit

---
+++ Ex1/Ex1.py
@@ -0,0 +1,16 @@
+import math
+def simple(n):
+    if n==1:
+        return True
+    for i in range(2,int(math.sqrt(n))+1):
+        if n % i == 0:
+             return False
+    return True
+res=[a for a in range(1,10**7) if simple(a)]
+f=open('text.txt','w')
+for x in range(0,10):
+    f.write(str(res.pop())+' ')
+f.close()
+
+
+
