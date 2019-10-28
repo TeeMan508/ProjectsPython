ff396c4c45823797a3ab9a81e25270077c409574
TimofeySh <TimofeySh@users.noreply.github.com>
2019-10-28 17:56:16 +0700

    Initial Commit

---
+++ Ex2/Ex2.py
@@ -0,0 +1,20 @@
+import numpy as np
+a=[]
+#a=np.array(x for x in f)
+with open('Input.txt') as ifile:
+    for line in ifile:
+        a.append(int(line))
+print(str(a)+'\n')
+def max(b=np.array([int(x) for x in a])):
+    res = 0
+    for x in b:
+        if x>res:
+            res=x
+    return res
+def min(b=np.array([int(x) for x in a])):
+    res= 0
+    for x in b:
+        if x<res:
+            res=x
+    return res
+print('max is: '+str(min(b))+' '+'min is:'+str(min(b)))
