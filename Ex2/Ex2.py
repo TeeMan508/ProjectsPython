47205acb2fdc0cc93f8cfb90a43842c1d7b307df
TeeMan508 <t.shshudro@g.nsu.ru>
2019-10-28 20:30:01 +0700

    Initial Commit

--- Ex2/Ex2.py
+++ Ex2/Ex2.py
@@ -1,22 +1,20 @@
 import numpy as np
 a=[]
-#a=np.array(x for x in f)
 with open('Input.txt') as ifile:
     for line in ifile:
         a.append(int(line))
-print(str(a)+'\n')
-def max(b=np.array([int(x) for x in a])):
-    res = 0
-    for x in b:
+b=np.array([int(x) for x in a])
+print(str(b)+'\n')
+def max(arg):
+    res =b[1]
+    for x in arg:
         if x>res:
             res=x
     return res
-def min(b=np.array([int(x) for x in a])):
-    res= 0
-    for x in b:
+def min(arg):
+    res=b[1]
+    for x in arg:
         if x<res:
             res=x
     return res
-print('max is: '+str(min(b))+' '+'min is:'+str(min(b)))
-
-#test commit
+print('max is:'+str(max(b))+' '+'min is:'+str(min(b)))
