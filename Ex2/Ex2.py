05d6dbba94271e6535ea79df00e7497528fed151
TeeMan508 <t.shshudro@g.nsu.ru>
2019-10-29 19:57:16 +0700

    Initial Commit1

--- Ex2/Ex2.py
+++ Ex2/Ex2.py
@@ -4,19 +4,8 @@ with open('Input.txt') as ifile:
     for line in ifile:
         a.append(int(line))
 b=np.array([int(x) for x in a])
-print(str(b)+'\n')
-def max(arg):
-    res =b[1]
-    for x in arg:
-        if x>res:
-            res=x
-    return res
-def min(arg):
-    res=b[1]
-    for x in arg:
-        if x<res:
-            res=x
-    return res
-print('max is:'+str(max(b))+' '+'min is:'+str(min(b)))
+bsort=sorted(b)
+print(str(bsort)+'\n')
+print('max is:',str(bsort[0]))
+print('max is:',str(bsort[len(bsort)-1]))
 
-#TestCommit
