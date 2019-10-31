43c2ec212dd33611ef37136759da695769154867
TeeMan508 <t.shshudro@g.nsu.ru>
2019-10-31 15:19:18 +0700

    Initial Commit

--- Ex2/Ex2.py
+++ Ex2/Ex2.py
@@ -4,11 +4,12 @@ with open('Input.txt') as ifile:
     for line in ifile:
         a.append(int(line))
 b=np.array([int(x) for x in a])
-bsort=sorted(b)
-print(str(bsort)+'\n')
-print('max is:',str(bsort[0]))
-print('max is:',str(bsort[len(bsort)-1]))
+print(str(sorted(b))+'\n')
+print('max is:',str(sorted(b)[0]))
+print('max is:',str(sorted(b)[len(sorted(b))-1]))
 print('mean5 is:',((b-b.mean())**5).mean())
 print('RMS is:',np.std(b).round(3))
+
+
 
 
