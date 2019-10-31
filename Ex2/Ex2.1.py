39d9e661b5d93adf42ae11051529e44fc7f92b28
TeeMan508 <t.shshudro@g.nsu.ru>
2019-10-31 22:18:05 +0700

    Initial Commit

--- Ex2/Ex2.1.py
+++ Ex2/Ex2.1.py
@@ -1,17 +1,20 @@
 import numpy as np
+import this
+print('\n')
 a=[]
 with open('Input.txt') as ifile:
     for line in ifile:
         a.append(int(line))
 b=np.array([int(x) for x in a])
-print(str(sorted(b))+'\n')
-print('max is:',str(sorted(b)[0]))
-print('mean is:',b.mean())
+print('Sorted array:',str(sorted(b)))
+print('min is:',str(sorted(b)[0]))
 print('max is:',str(sorted(b)[len(sorted(b))-1]))
-print('mean5 is:',((b-b.mean())**5).mean())
+print('mean is:',b.mean())
+print('mean^5 is:',((b-b.mean())**5).mean())
 print('RMS is:',np.std(b).round(3))
 
-complexb=np.genfromtxt('ComplexInput.txt', delimiter='\n', dtype=complex)
+
+complexb=tuple(np.genfromtxt('ComplexInput.txt', delimiter='\n', dtype=complex))
 print('Absolute sorting:',str(sorted(complexb, key=lambda x: abs(x))))
 print('Reverse real sorting:',str(sorted(complexb, key=lambda x: x.real, reverse=True)))
 
